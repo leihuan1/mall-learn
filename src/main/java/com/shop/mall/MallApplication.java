fc72543d5ed87ab457e21bff489e400365ec8b4b
huan <huan1379221079@163.com>
2019-09-28 23:33:53 +0800

    测试git

--- src/main/java/com/shop/mall/MallApplication.java
+++ src/main/java/com/shop/mall/MallApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MallApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MallApplication.class, args);
     }
